dbcf9436f21434a9dc51d0b6742051de96217b77
davout1805 <davout1805@gmail.com>
2013-09-04 01:51:49 -0400

    1

--- myScripts/CreateFCConditions.js
+++ myScripts/CreateFCConditions.js
@@ -6,6 +6,8 @@
  * To change this template use File | Settings | File Templates.
  */
 
+var DavoutConditions = DavoutConditions || {};
+
 on("ready", function() {
     state.davoutFcConditions = state.davoutFcConditions || [];
 
@@ -14,8 +16,20 @@ on("ready", function() {
 
 });
 
+DavoutConditions.getConditionsOnChar = function (charId){
+    return state["davoutFcConditions_" + charId];
+}
+
+DavoutConditions.addConditionToChar = function(charId, conditionName){
+    if (DavoutConditions.getConditionsOnChar(charId) == undefined){
+        state["davoutFcConditions_" + charId] = [conditionName];
+    } else {
+        state["davoutFcConditions_" + charId].push(conditionName);
+    }
+}
+
 on("chat:message", function(msg) {
-    if(msg.type == "api" && msg.who.indexOf("(GM)") !== -1 && msg.content.indexOf("!fat") !== -1){
+    if(msg.type == "api" && DavoutUtils.isSenderGm(msg) && msg.content.indexOf("!fat") !== -1){
         if (msg.selected == undefined){
             sendChat("API", "nothing is selected");
             return;
@@ -23,30 +37,31 @@ on("chat:message", function(msg) {
 
         _.each(msg.selected, function(obj){
             token = getObj("graphic", obj._id);
-            if (token.get("represents") !== "") {
-                state["davoutFcConditions_" + token.get("represents")] = "fatiguedI";
+            log("obj._id = " + obj._id);
+            var characterId = token.get("represents");
+            if (characterId !== "") {
+                DavoutConditions.addConditionToChar(characterId, "fatiguedI");
+                var conditionEffects = state.davoutFcConditions["fatiguedI"].effects;
+                _.each(conditionEffects, function(obj){
+                    log("obj.attrib = " + obj.attrib);
+                    DavoutUtils.adjustAttributeForChar(characterId, obj.attrib, obj.modifier);
+                });
             }
-
-        });
-
-        log("char state= " + state["davoutFcConditions_" + token.get("represents")]);
-        var conditionEffects = state.davoutFcConditions["fatiguedI"].effects;
-        _.each(conditionEffects, function(obj){
-            log("fat I condition= " + obj.attrib + " modified by " + obj.modifier);
         });
     }
 });
 
 on("chat:message", function(msg) {
-    if(msg.type == "api" && msg.who.indexOf("(GM)") !== -1 && msg.content.indexOf("!cur") !== -1 ){
+    if(msg.type == "api" && DavoutUtils.isSenderGm(msg) && msg.content.indexOf("!cur") !== -1 ){
+        var token;
+        var characterId;
         _.each(msg.selected, function(obj){
             token = getObj("graphic", obj._id);
-            if (token.get("represents") !== "") {
-                var strAttribute = findObjs({_type: "attribute", name: "str", _characterid: token.get("represents")})[0];
-                log("strAttribute = " + strAttribute.get("current"));
+            characterId = token.get("represents");
+            if (characterId !== "") {
+                var strAttribute = findObjs({_type: "attribute", name: "curStr", _characterid: characterId})[0];
+//                log("strAttribute = " + strAttribute.get("current"));
             }
-
         });
-
     }
 });
--- myScripts/CreateFcSheet.js
+++ myScripts/CreateFcSheet.js
@@ -1,22 +1,22 @@
-var davout = davout || {};
+var DavoutSheet = DavoutSheet || {};
 
-davout.attribs = [];
-davout.attribs["str"] = { name: "str" , current: 0};
-davout.attribs["dex"] = { name: "dex" , current: 0};
-davout.attribs["con"] = { name: "con" , current: 0};
-davout.attribs["int"] = { name: "int" , current: 0};
-davout.attribs["wis"] = { name: "wis" , current: 0};
-davout.attribs["cha"] = { name: "cha" , current: 0};
-davout.attribs["fort"] = { name: "Fortitude-Mod", current: 0, max: 0 };
-davout.attribs["ref"] = { name: "Reflex-Mod", current: 0, max: 0 };
-davout.attribs["will"] = { name: "Will-Mod", current: 0, max: 0 };
+DavoutSheet.attribs = [];
+DavoutSheet.attribs["str"] = { name: "str" , current: 0};
+DavoutSheet.attribs["dex"] = { name: "dex" , current: 0};
+DavoutSheet.attribs["con"] = { name: "con" , current: 0};
+DavoutSheet.attribs["int"] = { name: "int" , current: 0};
+DavoutSheet.attribs["wis"] = { name: "wis" , current: 0};
+DavoutSheet.attribs["cha"] = { name: "cha" , current: 0};
+DavoutSheet.attribs["fort"] = { name: "Fortitude-Mod", current: 0, max: 0 };
+DavoutSheet.attribs["ref"] = { name: "Reflex-Mod", current: 0, max: 0 };
+DavoutSheet.attribs["will"] = { name: "Will-Mod", current: 0, max: 0 };
 
-davout.skills = [
+DavoutSheet.skills = [
     { name: "acrobatics", attribute: "dex", acp: true },
     { name: "athletics", attribute: "str", acp: true }
 ];
 
-davout.saves = [];
-davout.saves["fort"] = { name: "fort-save", attribute: "con", bonus: davout.attribs["fort"].name };
-davout.saves["ref"] = { name: "ref-save", attribute: "dex", bonus: davout.attribs["ref"].name };
-davout.saves["will"] = { name: "will-save", attribute: "wis", bonus: davout.attribs["will"].name };
+DavoutSheet.saves = [];
+DavoutSheet.saves["fort"] = { name: "fort-save", attribute: "con", bonus: DavoutSheet.attribs["fort"].name };
+DavoutSheet.saves["ref"] = { name: "ref-save", attribute: "dex", bonus: DavoutSheet.attribs["ref"].name };
+DavoutSheet.saves["will"] = { name: "will-save", attribute: "wis", bonus: DavoutSheet.attribs["will"].name };
