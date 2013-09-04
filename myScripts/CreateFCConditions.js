7b260dabca88da5ca3e7a1000a962fb21a4b2b2a
davout1805 <davout1805@gmail.com>
2013-09-03 22:31:00 -0400

    Test commit

--- myScripts/CreateFCConditions.js
+++ myScripts/CreateFCConditions.js
@@ -6,7 +6,47 @@
  * To change this template use File | Settings | File Templates.
  */
 
-state.davoutFcConditions = state.davoutFcConditions || [];
+on("ready", function() {
+    state.davoutFcConditions = state.davoutFcConditions || [];
 
-state.davoutFcConditions["fatiguedI"] = {effect: [{attrib: "Str", modifier: -2}, {attrib: "Dex", modifier: -2}, {attrib: "Speed", modifier: -5}] };
-state.davoutFcConditions["fatiguedII"] = {effect: [{attrib: "Str", modifier: -4}, {attrib: "Dex", modifier: -4}, {attrib: "Speed", modifier: -10}] };
+    state.davoutFcConditions["fatiguedI"] = {effects: [{attrib: "str", modifier: -2}, {attrib: "dex", modifier: -2}, {attrib: "speed", modifier: -5}] };
+    state.davoutFcConditions["fatiguedII"] = {effects: [{attrib: "str", modifier: -4}, {attrib: "dex", modifier: -4}, {attrib: "speed", modifier: -10}] };
+
+});
+
+on("chat:message", function(msg) {
+    if(msg.type == "api" && msg.who.indexOf("(GM)") !== -1 && msg.content.indexOf("!fat") !== -1){
+        if (msg.selected == undefined){
+            sendChat("API", "nothing is selected");
+            return;
+        }
+
+        _.each(msg.selected, function(obj){
+            token = getObj("graphic", obj._id);
+            if (token.get("represents") !== "") {
+                state["davoutFcConditions_" + token.get("represents")] = "fatiguedI";
+            }
+
+        });
+
+        log("char state= " + state["davoutFcConditions_" + token.get("represents")]);
+        var conditionEffects = state.davoutFcConditions["fatiguedI"].effects;
+        _.each(conditionEffects, function(obj){
+            log("fat I condition= " + obj.attrib + " modified by " + obj.modifier);
+        });
+    }
+});
+
+on("chat:message", function(msg) {
+    if(msg.type == "api" && msg.who.indexOf("(GM)") !== -1 && msg.content.indexOf("!cur") !== -1 ){
+        _.each(msg.selected, function(obj){
+            token = getObj("graphic", obj._id);
+            if (token.get("represents") !== "") {
+                var strAttribute = findObjs({_type: "attribute", name: "str", _characterid: token.get("represents")})[0];
+                log("strAttribute = " + strAttribute.get("current"));
+            }
+
+        });
+
+    }
+});
--- myScripts/CreateFcSheet.js
+++ myScripts/CreateFcSheet.js
@@ -1,3 +1,22 @@
 var davout = davout || {};
 
 davout.attribs = [];
+davout.attribs["str"] = { name: "str" , current: 0};
+davout.attribs["dex"] = { name: "dex" , current: 0};
+davout.attribs["con"] = { name: "con" , current: 0};
+davout.attribs["int"] = { name: "int" , current: 0};
+davout.attribs["wis"] = { name: "wis" , current: 0};
+davout.attribs["cha"] = { name: "cha" , current: 0};
+davout.attribs["fort"] = { name: "Fortitude-Mod", current: 0, max: 0 };
+davout.attribs["ref"] = { name: "Reflex-Mod", current: 0, max: 0 };
+davout.attribs["will"] = { name: "Will-Mod", current: 0, max: 0 };
+
+davout.skills = [
+    { name: "acrobatics", attribute: "dex", acp: true },
+    { name: "athletics", attribute: "str", acp: true }
+];
+
+davout.saves = [];
+davout.saves["fort"] = { name: "fort-save", attribute: "con", bonus: davout.attribs["fort"].name };
+davout.saves["ref"] = { name: "ref-save", attribute: "dex", bonus: davout.attribs["ref"].name };
+davout.saves["will"] = { name: "will-save", attribute: "wis", bonus: davout.attribs["will"].name };
