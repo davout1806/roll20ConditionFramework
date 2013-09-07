2910d09012acfacd2a610b31e9c7fda20b7e7364
davout1805 <davout1805@gmail.com>
2013-09-07 12:25:46 -0400

    Create char sheet with secret rolls for saves & skills

--- myScripts/CreateFcSheet.js
+++ myScripts/CreateFcSheet.js
@@ -1,22 +1,208 @@
 var DavoutSheet = DavoutSheet || {};
 
+DavoutSheet.diceType = "1d20";
+
+//add init
+
 DavoutSheet.attribs = [];
-DavoutSheet.attribs["str"] = { name: "str" , current: 0};
-DavoutSheet.attribs["dex"] = { name: "dex" , current: 0};
-DavoutSheet.attribs["con"] = { name: "con" , current: 0};
-DavoutSheet.attribs["int"] = { name: "int" , current: 0};
-DavoutSheet.attribs["wis"] = { name: "wis" , current: 0};
-DavoutSheet.attribs["cha"] = { name: "cha" , current: 0};
+DavoutSheet.attribs["name"] = { name: "Name" , current: "", max: "" };
+DavoutSheet.attribs["str"] = { name: "Str" , current: 0, max: 0 };
+DavoutSheet.attribs["dex"] = { name: "Dex" , current: 0, max: 0 };
+DavoutSheet.attribs["con"] = { name: "Con" , current: 0, max: 0 };
+DavoutSheet.attribs["int"] = { name: "Int" , current: 0, max: 0 };
+DavoutSheet.attribs["wis"] = { name: "Wis" , current: 0, max: 0 };
+DavoutSheet.attribs["cha"] = { name: "Cha" , current: 0, max: 0 };
 DavoutSheet.attribs["fort"] = { name: "Fortitude-Mod", current: 0, max: 0 };
 DavoutSheet.attribs["ref"] = { name: "Reflex-Mod", current: 0, max: 0 };
 DavoutSheet.attribs["will"] = { name: "Will-Mod", current: 0, max: 0 };
-
-DavoutSheet.skills = [
-    { name: "acrobatics", attribute: "dex", acp: true },
-    { name: "athletics", attribute: "str", acp: true }
-];
+DavoutSheet.attribs["acp"] = { name: "AC-Penalty", current: 0, max: 0 };
+DavoutSheet.attribs["spd"] = { name: "Speed", current: 30, max: 30 };
+DavoutSheet.attribs["acrobaticsBase"] = { name: "Acrobatics-Base", current: 0, max: 0 };
+DavoutSheet.attribs["athleticsBase"] = { name: "Athletics-Base", current: 0, max: 0 };
+DavoutSheet.attribs["blendBase"] = { name: "Blend-Base", current: 0, max: 0 };
+DavoutSheet.attribs["bluffBase"] = { name: "Bluff-Base", current: 0, max: 0 };
+DavoutSheet.attribs["craftingBase"] = { name: "Crafting-Base", current: 0, max: 0 };
+DavoutSheet.attribs["haggleBase"] = { name: "Haggle-Base", current: 0, max: 0 };
+DavoutSheet.attribs["impressBase"] = { name: "Impress-Base", current: 0, max: 0 };
+DavoutSheet.attribs["intimidateBase"] = { name: "Intimidate-Base", current: 0, max: 0 };
+DavoutSheet.attribs["investigateBase"] = { name: "Investigate-Base", current: 0, max: 0 };
+DavoutSheet.attribs["medicineBase"] = { name: "Medicine-Base", current: 0, max: 0 };
+DavoutSheet.attribs["noticeBase"] = { name: "Notice-Base", current: 0, max: 0 };
+DavoutSheet.attribs["prestidigitationBase"] = { name: "Prestidigitation-Base", current: 0, max: 0 };
+DavoutSheet.attribs["resolveBase"] = { name: "Resolve-Base", current: 0, max: 0 };
+DavoutSheet.attribs["rideBase"] = { name: "Ride-Base", current: 0, max: 0 };
+DavoutSheet.attribs["searchBase"] = { name: "Search-Base", current: 0, max: 0 };
+DavoutSheet.attribs["senseMotiveBase"] = { name: "Sense-Motive-Base", current: 0, max: 0 };
+DavoutSheet.attribs["sneakBase"] = { name: "Sneak-Base", current: 0, max: 0 };
+DavoutSheet.attribs["survivalBase"] = { name: "Survival-Base", current: 0, max: 0 };
+DavoutSheet.attribs["tacticsBase"] = { name: "Tactics-Base", current: 0, max: 0 };
 
 DavoutSheet.saves = [];
-DavoutSheet.saves["fort"] = { name: "fort-save", attribute: "con", bonus: DavoutSheet.attribs["fort"].name };
-DavoutSheet.saves["ref"] = { name: "ref-save", attribute: "dex", bonus: DavoutSheet.attribs["ref"].name };
-DavoutSheet.saves["will"] = { name: "will-save", attribute: "wis", bonus: DavoutSheet.attribs["will"].name };
+DavoutSheet.saves["fort"] = { name: "Fort-Save", attribute: "Con", bonus: DavoutSheet.attribs["fort"].name };
+DavoutSheet.saves["ref"] = { name: "Ref-Save", attribute: "Dex", bonus: DavoutSheet.attribs["ref"].name };
+DavoutSheet.saves["will"] = { name: "Will-Save", attribute: "Wis", bonus: DavoutSheet.attribs["will"].name };
+
+DavoutSheet.skills = [
+    { name: "Acrobatics", attribute: DavoutSheet.attribs["dex"].name, rank: DavoutSheet.attribs["acrobaticsBase"].name, acp: true },
+    { name: "Athletics", attribute: DavoutSheet.attribs["str"].name, rank: DavoutSheet.attribs["athleticsBase"].name, acp: true },
+    { name: "Blend", attribute: DavoutSheet.attribs["cha"].name, rank: DavoutSheet.attribs["blendBase"].name, acp: false },
+    { name: "Bluff", attribute: DavoutSheet.attribs["cha"].name, rank: DavoutSheet.attribs["bluffBase"].name, acp: false },
+    { name: "Crafting", attribute: DavoutSheet.attribs["int"].name, rank: DavoutSheet.attribs["craftingBase"].name, acp: false },
+    { name: "Haggle", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["haggleBase"].name, acp: false },
+    { name: "Impress", attribute: DavoutSheet.attribs["cha"].name, rank: DavoutSheet.attribs["impressBase"].name, acp: false },
+    { name: "Intimidate", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["intimidateBase"].name, acp: false },
+    { name: "Investigate", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["investigateBase"].name, acp: false },
+    { name: "Medicine", attribute: DavoutSheet.attribs["int"].name, rank: DavoutSheet.attribs["medicineBase"].name, acp: false },
+    { name: "Notice", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["noticeBase"].name, acp: false },
+    { name: "Prestidigitation", attribute: DavoutSheet.attribs["dex"].name, rank: DavoutSheet.attribs["prestidigitationBase"].name, acp: true },
+    { name: "Resolve", attribute: DavoutSheet.attribs["con"].name, rank: DavoutSheet.attribs["resolveBase"].name, acp: false },
+    { name: "Ride", attribute: DavoutSheet.attribs["dex"].name, rank: DavoutSheet.attribs["rideBase"].name, acp: true },
+    { name: "Search", attribute: DavoutSheet.attribs["int"].name, rank: DavoutSheet.attribs["searchBase"].name, acp: false },
+    { name: "Sense-Motive", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["senseMotiveBase"].name, acp: false },
+    { name: "Sneak", attribute: DavoutSheet.attribs["dex"].name, rank: DavoutSheet.attribs["sneakBase"].name, acp: true },
+    { name: "Survival", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["survivalBase"].name, acp: false },
+    { name: "Tactics", attribute: DavoutSheet.attribs["int"].name, rank: DavoutSheet.attribs["tacticsBase"].name, acp: false }
+
+];
+
+DavoutSheet.processAttribs = function processAttribs(character) {
+    for (var index in DavoutSheet.attribs) {
+        createObj("attribute", {
+            name: DavoutSheet.attribs[index].name,
+            current: DavoutSheet.attribs[index].current,
+            max: DavoutSheet.attribs[index].max,
+            characterid: character.id
+        });
+    };
+}
+
+DavoutSheet.getSkillBonus = function getSkillBonus(skill, charType) {
+    log("Skill: " + JSON.stringify(skill));
+    if (_.indexOf(skill.classSkill, charType) !== -1
+        || _.indexOf(skill.classSkill, 'all') !== -1) {
+        return "+@{" + DavoutSheet.attribs["csb"].name + "}";
+    }
+    return "";
+}
+
+DavoutSheet.createSaveAction = function createSaveAction(save) {
+    var saveAction = "/w gm @{Name} ";
+    saveAction += save.name;
+    saveAction += ": [[@{" + save.attribute + "}";
+    saveAction += "+@{" + save.bonus + "}+";
+    saveAction += DavoutSheet.diceType;
+    saveAction += "]]";
+    return saveAction;
+}
+
+DavoutSheet.createAbilityAction = function createAbilityAction(skill, charType) {
+    var abilityAction = "/w gm @{Name} ";
+    abilityAction += skill.name;
+    abilityAction += ": [[@{" + skill.attribute + "}";
+    abilityAction += "+@{" + skill.rank + "}";
+    abilityAction += DavoutSheet.getSkillBonus(skill, charType);
+    if (skill.acp) {
+        abilityAction += "+@{" + DavoutSheet.attribs["acp"].name + "}";
+    }
+    abilityAction += "+" + DavoutSheet.diceType;
+    abilityAction += "]]";
+    return abilityAction;
+}
+
+DavoutSheet.retrieveUserModifiers = function retrieveUserModifiers(currentSkill, actionCommand) {
+    var modifier = actionCommand.replace(currentSkill.name+"\n/r " + DavoutSheet.diceType + "+@{" + currentSkill.attribute + "}", "");
+    log("Modifier - Step 1: " + modifier);
+    modifier = modifier.replace("+@{" + DavoutSheet.attribs["csb"].name + "}", "");
+    log("Modifier - Step 2: " + modifier);
+    modifier = modifier.replace("+@{" + DavoutSheet.attribs["acp"].name + "}", "");
+    log("Modifier - Step 3: " + modifier);
+    return modifier;
+}
+
+DavoutSheet.processSkills = function processSkills(character, charType) {
+    for (var save in DavoutSheet.saves) {
+        var saveAction = DavoutSheet.createSaveAction(DavoutSheet.saves[save]);
+        var savingthrows = findObjs({
+            name: DavoutSheet.saves[save].name,
+            description: "",
+            type: "ability",
+            characterid: character.id
+        }, {caseInsensitive: true});
+        if (savingthrows.length > 0) {
+            _.each(savingthrows, function(save){
+                save.set("action", saveAction);
+            });
+        } else {
+            createObj("ability", {
+                name: DavoutSheet.saves[save].name,
+                description: "",
+                action: saveAction,
+                characterid: character.id
+            });
+        }
+    };
+
+    var placeHolder = findObjs({
+        name: "--------------------------",
+        description: "",
+        action: "",
+        type: "ability",
+        characterid: character.id
+    }, {caseInsensitive: true});
+    if (placeHolder.length < 1) {
+        createObj("ability", {
+            name: "--------------------------",
+            description: "",
+            action: "",
+            characterid: character.id
+        });
+    }
+    
+    for (var skill in DavoutSheet.skills) {
+        var abilityAction = DavoutSheet.createAbilityAction(DavoutSheet.skills[skill], charType);
+        var abilities = findObjs({
+            name: DavoutSheet.skills[skill].name,
+            type: "ability",
+            description: "",
+            characterid: character.id
+        }, {caseInsensitive: true});
+        if (abilities.length > 0) {
+            _.each(abilities, function(ability) {
+                var userModifier = DavoutSheet.retrieveUserModifiers(DavoutSheet.skills[skill], ability.get("action"));
+                log("Retrieved User-Modifier: " + userModifier);
+                abilityAction += userModifier;
+                ability.set("action", abilityAction);
+            })
+        } else {
+            createObj("ability", {
+                name: DavoutSheet.skills[skill].name,
+                description: "",
+                action: abilityAction,
+                characterid: character.id
+            });
+        }
+    };
+
+    var placeHolder = findObjs({
+        name: "--------------------------",
+        description: "",
+        action: "",
+        type: "ability",
+        characterid: character.id
+    }, {caseInsensitive: true});
+    if (placeHolder.length < 1) {
+        createObj("ability", {
+            name: "--------------------------",
+            description: "",
+            action: "",
+            characterid: character.id
+        });
+    }
+}
+
+on("ready", function() {
+    on("add:character", function(char) {
+        DavoutSheet.processAttribs(char);
+        DavoutSheet.processSkills(char, 'all');
+    });
+});
--- othersScripts/Malikai-mgruel/CreatePathfinderCharacterSheet.js
+++ othersScripts/Malikai-mgruel/CreatePathfinderCharacterSheet.js
@@ -1,7 +1,5 @@
-/*
- * roll20 User: https://app.roll20.net/users/2267/malikai
- * git User:    https://gist.github.com/mgruel/
- */
+// This is Malikai script with a few addons
+
 var malikai = malikai || {};
 
 malikai.diceType = "1d20";
@@ -19,7 +17,16 @@ malikai.charClasses = [
     'rogue',
     'sorcerer',
     'summoner',
-    'wizard'
+    'wizard',
+    'gunslinger',
+    'ninja',
+    'samurai',
+    'magus',
+    'alchemist',
+    'cavalier',
+    'inquisitor',
+    'witch'
+
 ];
 
 malikai.attribs = [];
@@ -43,43 +50,78 @@ malikai.attribs["bab"] = { name: "BAB", current: 0, max: 0 };
 malikai.attribs["csb"] = { name: "Class-Skill-Bonus", current: 3, max: 3 };
 malikai.attribs["acpan"] = { name: "AC-Penalty", current: 0, max: 0 };
 malikai.attribs["spd"] = { name: "Speed", current: 30, max: 30 };
+malikai.attribs["acrobaticsRanks"] = { name: "Acrobatics-Ranks", current: 0, max: 0 };
+malikai.attribs["appraiseRanks"] = { name: "Appraise-Ranks", current: 0, max: 0 };
+malikai.attribs["bluffRanks"] = { name: "Bluff-Ranks", current: 0, max: 0 };
+malikai.attribs["climbRanks"] = { name: "Climb-Ranks", current: 0, max: 0 };
+malikai.attribs["craftRanks"] = { name: "Craft-Ranks", current: 0, max: 0 };
+malikai.attribs["diplomacyRanks"] = { name: "Diplomacy-Ranks", current: 0, max: 0 };
+malikai.attribs["disableDeviceRanks"] = { name: "Disable-Device-Ranks", current: 0, max: 0 };
+malikai.attribs["disguiseRanks"] = { name: "Disguise-Ranks", current: 0, max: 0 };
+malikai.attribs["escapeArtistRanks"] = { name: "Escape-Artist-Ranks", current: 0, max: 0 };
+malikai.attribs["flyRanks"] = { name: "Fly-Ranks", current: 0, max: 0 };
+malikai.attribs["handleAnimalRanks"] = { name: "Handle-Animal-Ranks", current: 0, max: 0 };
+malikai.attribs["healRanks"] = { name: "Heal-Ranks", current: 0, max: 0 };
+malikai.attribs["intimidateRanks"] = { name: "Intimidate-Ranks", current: 0, max: 0 };
+malikai.attribs["knowledgeArcanaRanks"] = { name: "Knowledge-Arcana-Ranks", current: 0, max: 0 };
+malikai.attribs["knowledgeDungeoneeringRanks"] = { name: "Knowledge-Dungeoneering-Ranks", current: 0, max: 0 };
+malikai.attribs["knowledgeEngineeringRanks"] = { name: "Knowledge-Engineering-Ranks", current: 0, max: 0 };
+malikai.attribs["knowledgeGeographyRanks"] = { name: "Knowledge-Geography-Ranks", current: 0, max: 0 };
+malikai.attribs["knowledgeHistoryRanks"] = { name: "Knowledge-History-Ranks", current: 0, max: 0 };
+malikai.attribs["knowledgeLocalRanks"] = { name: "Knowledge-Local-Ranks", current: 0, max: 0 };
+malikai.attribs["knowledgeNatureRanks"] = { name: "Knowledge-Nature-Ranks", current: 0, max: 0 };
+malikai.attribs["knowledgeNobilityRanks"] = { name: "Knowledge-Nobility-Ranks", current: 0, max: 0 };
+malikai.attribs["knowledgePlanesRanks"] = { name: "Knowledge-Planes-Ranks", current: 0, max: 0 };
+malikai.attribs["knowledgeReligionRanks"] = { name: "Knowledge-Religion-Ranks", current: 0, max: 0 };
+malikai.attribs["linguisticsRanks"] = { name: "Linguistics-Ranks", current: 0, max: 0 };
+malikai.attribs["perceptionRanks"] = { name: "Perception-Ranks", current: 0, max: 0 };
+malikai.attribs["performRanks"] = { name: "Perform-Ranks", current: 0, max: 0 };
+malikai.attribs["professionRanks"] = { name: "Profession-Ranks", current: 0, max: 0 };
+malikai.attribs["rideRanks"] = { name: "Ride-Ranks", current: 0, max: 0 };
+malikai.attribs["senseMotiveRanks"] = { name: "Sense-Motive-Ranks", current: 0, max: 0 };
+malikai.attribs["sleightOfHandRanks"] = { name: "Sleight-of-Hand-Ranks", current: 0, max: 0 };
+malikai.attribs["spellcraftRanks"] = { name: "Spellcraft-Ranks", current: 0, max: 0 };
+malikai.attribs["stealthRanks"] = { name: "Stealth-Ranks", current: 0, max: 0 };
+malikai.attribs["survivalRanks"] = { name: "Survival-Ranks", current: 0, max: 0 };
+malikai.attribs["swimRanks"] = { name: "Swim-Ranks", current: 0, max: 0 };
+malikai.attribs["useMagicDeviceRanks"] = { name: "Use-Magic-Device-Ranks", current: 0, max: 0 };
 
 malikai.skills = [
-    { name: "Acrobatics", attribute: malikai.attribs["dex"].name, acpan: true, classSkill: ['barbarian', 'bard', 'cleric', 'monk', 'rogue'] },
-    { name: "Appraise", attribute: malikai.attribs["int"].name, classSkill: ['bard', 'cleric', 'rogue', 'sorcerer', 'wizard'] },
-    { name: "Bluff", attribute: malikai.attribs["cha"].name, classSkill: ['bard','rogue','sorcerer'] },
-    { name: "Climb", attribute: malikai.attribs["str"].name, acpan: true, classSkill: ['barbarian','bard','druid','fighter','monk','ranger','rogue'] },
-    { name: "Craft", attribute: malikai.attribs["int"].name, classSkill: ['all'] },
-    { name: "Diplomacy", attribute: malikai.attribs["cha"].name, classSkill: ['bard','cleric','oracle','paladin','rogue'] },
-    { name: "Disable-Device", attribute: malikai.attribs["dex"].name, acpan: true, classSkill: ['rogue'] },
-    { name: "Disguise", attribute: malikai.attribs["cha"].name, classSkill: ['bard','rogue'] },
-    { name: "Escape-Artist", attribute: malikai.attribs["dex"].name, acpan: true, classSkill: ['bard','monk','rogue'] },
-    { name: "Fly", attribute: malikai.attribs["dex"].name, acpan: true, classSkill: ['druid','summoner','sorcerer','wizard'] },
-    { name: "Handle-Animal", attribute: malikai.attribs["cha"].name, classSkill: ['barbarian','druid','fighter','paladin','ranger','summoner'] },
-    { name: "Heal", attribute: malikai.attribs["wis"].name, classSkill: ['cleric','druid','oracle','paladin','ranger'] },
-    { name: "Intimidate", attribute: malikai.attribs["cha"].name, classSkill: ['barbarian','bard','fighter','monk','ranger','rogue','sorcerer'] },
-    { name: "Knowledge-Arcana", attribute: malikai.attribs["int"].name, classSkill: ['bard','cleric','sorcerer','summoner','wizard'] },
-    { name: "Knowledge-Dungeoneering", attribute: malikai.attribs["int"].name, classSkill: ['bard','fighter','ranger','rogue','summoner','wizard'] },
-    { name: "Knowledge-Engineering", attribute: malikai.attribs["int"].name, classSkill: ['bard','fighter','summoner','wizard'] },
-    { name: "Knowledge-Geography", attribute: malikai.attribs["int"].name, classSkill: ['bard','druid','ranger','summoner','wizard'] },
-    { name: "Knowledge-History", attribute: malikai.attribs["int"].name, classSkill: ['bard','cleric','monk','oracle','summoner','wizard'] },
-    { name: "Knowledge-Local", attribute: malikai.attribs["int"].name, classSkill: ['bard','rogue','summoner','wizard'] },
-    { name: "Knowledge-Nature", attribute: malikai.attribs["int"].name, classSkill: ['barbarian','bard','druid','ranger','summoner','wizard'] },
-    { name: "Knowledge-Nobility", attribute: malikai.attribs["int"].name, classSkill: ['bard','cleric','paladin','summoner','wizard'] },
-    { name: "Knowledge-Planes", attribute: malikai.attribs["int"].name, classSkill: ['bard','cleric','oracle','summoner','wizard'] },
-    { name: "Knowledge-Religion", attribute: malikai.attribs["int"].name, classSkill: ['bard','cleric','monk','oracle','paladin','summoner','wizard'] },
-    { name: "Linguistics", attribute: malikai.attribs["int"].name, classSkill: ['bard','cleric','rogue','summoner','wizard'] },
-    { name: "Perception", attribute: malikai.attribs["wis"].name, classSkill: ['barbarian','bard','druid','monk','ranger','rogue'] },
-    { name: "Perform", attribute: malikai.attribs["cha"].name, classSkill: ['bard','monk','rogue'] },
-    { name: "Profession", attribute: malikai.attribs["cha"].name, classSkill: ['bard','cleric','druid','fighter','monk','oracle','paladin','ranger','rogue','sorcerer','summoner','wizard'] },
-    { name: "Ride", attribute: malikai.attribs["dex"].name, acpan: true, classSkill: ['barbarian','druid','fighter','monk','paladin','summoner','ranger'] },
-    { name: "Sense-Motive", attribute: malikai.attribs["wis"].name, classSkill: ['bard','cleric','monk','oracle','paladin','rogue'] },
-    { name: "Sleight-of-Hand", attribute: malikai.attribs["dex"].name, acpan: true, classSkill: ['bard','rogue'] },
-    { name: "Spellcraft", attribute: malikai.attribs["int"].name, classSkill: ['bard','cleric','druid','oracle','paladin','ranger','sorcerer','summoner','wizard'] },
-    { name: "Stealth", attribute: malikai.attribs["dex"].name, acpan: true, classSkill: ['bard','monk','ranger','rogue'] },
-    { name: "Survival", attribute: malikai.attribs["wis"].name, classSkill: ['barbarian','druid','fighter','ranger'] },
-    { name: "Swim", attribute: malikai.attribs["str"].name, acpan: true, classSkill: ['barbarian','druid','fighter','monk','ranger','rogue'] },
-    { name: "Use-Magic-Device", attribute: malikai.attribs["cha"].name, classSkill: ['bard','rogue','sorcerer','summoner'] }
+    { name: "Acrobatics", attribute: malikai.attribs["dex"].name, rank: malikai.attribs["acrobaticsRanks"].name, acpan: true, classSkill: ['barbarian', 'bard', 'cleric', 'monk', 'rogue','gunslinger','ninja'] },
+    { name: "Appraise", attribute: malikai.attribs["int"].name, rank: malikai.attribs["appraiseRanks"].name, classSkill: ['bard', 'cleric', 'rogue', 'sorcerer', 'wizard','ninja','alchemist'] },
+    { name: "Bluff", attribute: malikai.attribs["cha"].name, rank: malikai.attribs["bluffRanks"].name, classSkill: ['bard','rogue','sorcerer','gunslinger','ninja','samurai','cavalier','inquisitor'] },
+    { name: "Climb", attribute: malikai.attribs["str"].name, rank: malikai.attribs["climbRanks"].name, acpan: true, classSkill: ['barbarian','bard','druid','fighter','monk','ranger','rogue','gunslinger','ninja','samurai','magus','cavalier','inquisitor'] },
+    { name: "Craft", attribute: malikai.attribs["int"].name, rank: malikai.attribs["craftRanks"].name, classSkill: ['all'] },
+    { name: "Diplomacy", attribute: malikai.attribs["cha"].name, rank: malikai.attribs["diplomacyRanks"].name, classSkill: ['bard','cleric','oracle','paladin','rogue','ninja','samurai','cavalier','inquisitor'] },
+    { name: "Disable-Device", attribute: malikai.attribs["dex"].name, rank: malikai.attribs["disableDeviceRanks"].name, acpan: true, classSkill: ['rogue','ninja','alchemist'] },
+    { name: "Disguise", attribute: malikai.attribs["cha"].name, rank: malikai.attribs["disguiseRanks"].name, classSkill: ['bard','rogue','ninja','inquisitor'] },
+    { name: "Escape-Artist", attribute: malikai.attribs["dex"].name, rank: malikai.attribs["escapeArtistRanks"].name, acpan: true, classSkill: ['bard','monk','rogue','ninja'] },
+    { name: "Fly", attribute: malikai.attribs["dex"].name, rank: malikai.attribs["flyRanks"].name, acpan: true, classSkill: ['druid','summoner','sorcerer','wizard','magus','alchemist','witch'] },
+    { name: "Handle-Animal", attribute: malikai.attribs["cha"].name, rank: malikai.attribs["handleAnimalRanks"].name,  classSkill: ['barbarian','druid','fighter','paladin','ranger','summoner','gunslinger','samurai','cavalier'] },
+    { name: "Heal", attribute: malikai.attribs["wis"].name, rank: malikai.attribs["healRanks"].name, classSkill: ['cleric','druid','oracle','paladin','ranger','alchemist','inquisitor','witch'] },
+    { name: "Intimidate", attribute: malikai.attribs["cha"].name, rank: malikai.attribs["intimidateRanks"].name, classSkill: ['barbarian','bard','fighter','monk','ranger','rogue','sorcerer','gunslinger','ninja','samurai','magus','cavalier','inquisitor','witch'] },
+    { name: "Knowledge-Arcana", attribute: malikai.attribs["int"].name, rank: malikai.attribs["knowledgeArcanaRanks"].name, classSkill: ['bard','cleric','sorcerer','summoner','wizard','alchemist','inquisitor','witch'] },
+    { name: "Knowledge-Dungeoneering", attribute: malikai.attribs["int"].name, rank: malikai.attribs["knowledgeDungeoneeringRanks"].name, classSkill: ['bard','fighter','ranger','rogue','summoner','wizard','magus','inquisitor'] },
+    { name: "Knowledge-Engineering", attribute: malikai.attribs["int"].name, rank: malikai.attribs["knowledgeEngineeringRanks"].name, classSkill: ['bard','fighter','summoner','wizard','gunslinger'] },
+    { name: "Knowledge-Geography", attribute: malikai.attribs["int"].name, rank: malikai.attribs["knowledgeGeographyRanks"].name, classSkill: ['bard','druid','ranger','summoner','wizard'] },
+    { name: "Knowledge-History", attribute: malikai.attribs["int"].name, rank: malikai.attribs["knowledgeHistoryRanks"].name, classSkill: ['bard','cleric','monk','oracle','summoner','wizard','witch'] },
+    { name: "Knowledge-Local", attribute: malikai.attribs["int"].name, rank: malikai.attribs["knowledgeLocalRanks"].name, classSkill: ['bard','rogue','summoner','wizard','gunslinger','ninja'] },
+    { name: "Knowledge-Nature", attribute: malikai.attribs["int"].name, rank: malikai.attribs["knowledgeNatureRanks"].name, classSkill: ['barbarian','bard','druid','ranger','summoner','wizard','alchemist','inquisitor'] },
+    { name: "Knowledge-Nobility", attribute: malikai.attribs["int"].name, rank: malikai.attribs["knowledgeNobilityRanks"].name, classSkill: ['bard','cleric','paladin','summoner','wizard','ninja'] },
+    { name: "Knowledge-Planes", attribute: malikai.attribs["int"].name, rank: malikai.attribs["knowledgePlanesRanks"].name, classSkill: ['bard','cleric','oracle','summoner','wizard','magus','inquisitor','witch'] },
+    { name: "Knowledge-Religion", attribute: malikai.attribs["int"].name, rank: malikai.attribs["knowledgeReligionRanks"].name, classSkill: ['bard','cleric','monk','oracle','paladin','summoner','wizard','inquisitor'] },
+    { name: "Linguistics", attribute: malikai.attribs["int"].name, rank: malikai.attribs["linguisticsRanks"].name, classSkill: ['bard','cleric','rogue','summoner','wizard','ninja'] },
+    { name: "Perception", attribute: malikai.attribs["wis"].name, rank: malikai.attribs["perceptionRanks"].name, classSkill: ['barbarian','bard','druid','monk','ranger','rogue','gunslinger','ninja','alchemist','inquisitor'] },
+    { name: "Perform", attribute: malikai.attribs["cha"].name, rank: malikai.attribs["performRanks"].name, classSkill: ['bard','monk','rogue','ninja'] },
+    { name: "Profession", attribute: malikai.attribs["cha"].name, rank: malikai.attribs["professionRanks"].name, classSkill: ['bard','cleric','druid','fighter','monk','oracle','paladin','ranger','rogue','sorcerer','summoner','wizard','gunslinger','ninja','samurai','magus','alchemist','cavalier','inquisitor','witch'] },
+    { name: "Ride", attribute: malikai.attribs["dex"].name, rank: malikai.attribs["rideRanks"].name, acpan: true, classSkill: ['barbarian','druid','fighter','monk','paladin','summoner','ranger','gunslinger','samurai','magus','cavalier','inquisitor'] },
+    { name: "Sense-Motive", attribute: malikai.attribs["wis"].name, rank: malikai.attribs["senseMotiveRanks"].name, classSkill: ['bard','cleric','monk','oracle','paladin','rogue','ninja','samurai','cavalier','inquisitor'] },
+    { name: "Sleight-of-Hand", attribute: malikai.attribs["dex"].name, rank: malikai.attribs["sleightOfHandRanks"].name, acpan: true, classSkill: ['bard','rogue','gunslinger','ninja','alchemist'] },
+    { name: "Spellcraft", attribute: malikai.attribs["int"].name, rank: malikai.attribs["spellcraftRanks"].name, classSkill: ['bard','cleric','druid','oracle','paladin','ranger','sorcerer','summoner','wizard','magus','alchemist','inquisitor','witch'] },
+    { name: "Stealth", attribute: malikai.attribs["dex"].name, rank: malikai.attribs["stealthRanks"].name, acpan: true, classSkill: ['bard','monk','ranger','rogue','ninja','inquisitor'] },
+    { name: "Survival", attribute: malikai.attribs["wis"].name, rank: malikai.attribs["survivalRanks"].name, classSkill: ['barbarian','druid','fighter','ranger','gunslinger','alchemist','inquisitor'] },
+    { name: "Swim", attribute: malikai.attribs["str"].name, rank: malikai.attribs["swimRanks"].name, acpan: true, classSkill: ['barbarian','druid','fighter','monk','ranger','rogue','gunslinger','ninja','samurai','magus','cavalier','inquisitor'] },
+    { name: "Use-Magic-Device", attribute: malikai.attribs["cha"].name, rank: malikai.attribs["useMagicDeviceRanks"].name, classSkill: ['bard','rogue','sorcerer','summoner','ninja','magus','alchemist','witch'] }
 ];
 
 malikai.saves = [];
@@ -112,6 +154,7 @@ malikai.createAbilityAction = function createAbilityAction(skill, charType) {
     abilityAction += "\n/r ";
     abilityAction += malikai.diceType;
     abilityAction += "+@{" + skill.attribute + "}";
+    abilityAction += "+@{" + skill.rank + "}";
     abilityAction += malikai.getSkillBonus(skill, charType);
     if (skill.acpan) {
         abilityAction += "+@{" + malikai.attribs["acpan"].name + "}";
