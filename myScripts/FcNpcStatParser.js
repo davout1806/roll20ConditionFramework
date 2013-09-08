ef3dcd8aebbc7d7ea0ca7fa968f5ac68bf8634ae
davout1805 <davout1805@gmail.com>
2013-09-07 21:39:36 -0400

    Parsing Name and Attributes

--- myScripts/FcNpcStatParser.js
+++ myScripts/FcNpcStatParser.js
@@ -2,23 +2,68 @@ var DavoutNpcStatParser = DavoutNpcStatParser || {};
 DavoutNpcStatParser.command = DavoutNpcStatParser.command || {};
 
 DavoutNpcStatParser.parseIntoCharStats = function (text) {
+    var charStats = {};
+
     text = unescape(text);
     text = text.replace("<strong>", "");
     text = text.replace("</strong>", "");
-    var lines = text.split("<br>");  // \n in GM Notes.
+    var lines = text.split("<br>");  // \n in GM Notes after unescape.
 
-    _.each(lines, function (line) {
-        log(line);
-    });
-//    _.each(lines, function (line, lineIndex) {
-//        this[lineIndex] = line.split(" ");
-//    }, lines);
-//
-//    _.each(lines, function (line, lineIndex) {
-//        _.each(line, function (word, lineIndex) {
-//            log(word);
-//        } );
-//    } );
+    charStats.name = DavoutNpcStatParser.findName(lines[0]);
+//    log("name = " + charStats.name);
+    charStats.attribs = DavoutNpcStatParser.findAttributes(lines[1]);
+
+    return charStats;
+};
+
+DavoutNpcStatParser.findName = function(line){
+    var words = line.split(" ");
+    var nameWordStart;
+
+    for (var i=0; i < words.length && nameWordStart == undefined; i++){
+        if (words[i].indexOf(")") == -1){
+//            log("1. words[i] = " + words[i]);
+            nameWordStart = i;
+        }
+    }
+//    log("nameWordStart = " + nameWordStart);
+
+    var wordAfterName
+    var i = nameWordStart;
+    for (; i < words.length && wordAfterName == undefined; i++){
+        if (words[i].indexOf("(") != -1){
+//            log("2. words[i] = " + words[i]);
+            wordAfterName = i;
+        }
+    }
+//    log("wordAfterName = " + wordAfterName);
+    if (wordAfterName === null){
+        wordAfterName = nameWordStart + 1;
+    }
+//    log("wordAfterName = " + wordAfterName);
+
+    var name = "";
+    for (var i = nameWordStart; i < wordAfterName; i++){
+        name += words[i] + " ";
+    }
+
+    return name.trim();
+};
+
+DavoutNpcStatParser.findAttributes = function (line) {
+//    log("JSON.stringify(line) = " + JSON.stringify(line));
+    var attribs = {};
+    var attribPair = line.split(";");
+    log("JSON.stringify(attribPair) = " + JSON.stringify(attribPair));
+    for(var i=0; i < 6; i++){
+        var splitPair = attribPair[i].split(": ");
+        log("JSON.stringify(splitPair) = " + JSON.stringify(splitPair));
+        var splitValue = splitPair[1].split("/");
+        attribs[splitPair[0]] = splitValue[1];
+        log("JSON.stringify(splitValue[1]) = " + JSON.stringify(splitValue[1]));
+    }
+    log("attribs = " + JSON.stringify(attribs));
+    return attribs;
 };
 
 DavoutNpcStatParser.insertIntoSheet = function (charStats) {
@@ -27,7 +72,6 @@ DavoutNpcStatParser.insertIntoSheet = function (charStats) {
 
 DavoutNpcStatParser.command._parse = function (selected, npcType, isGm, msg){
     if (!DavoutUtils.checkForSelectionAndSendIfNothing(selected, "/w gm nothing is selected") && isGm) {
-        log("no");
         return;
     }
 
@@ -42,7 +86,9 @@ DavoutNpcStatParser.command._parse = function (selected, npcType, isGm, msg){
         tokenObjR20 = getObj("graphic", obj._id);
         characterId = tokenObjR20.get("represents");
         if (characterId !== "") {
-            DavoutNpcStatParser.insertIntoSheet(DavoutNpcStatParser.parseIntoCharStats(tokenObjR20.get("gmnotes")));
+//            DavoutNpcStatParser.insertIntoSheet(DavoutNpcStatParser.parseIntoCharStats(tokenObjR20.get("gmnotes")));
+            var x =DavoutNpcStatParser.parseIntoCharStats(tokenObjR20.get("gmnotes"));
+            log("x = " + JSON.stringify(x));
         } else {
             sendChat("API", tokenObjR20.get("name") + " is not a character.");
         }
@@ -70,3 +116,6 @@ on("ready", function () {
     };
     community.command.add("npc_parse", parseCommand);
 });
+
+// FcNpcStatParser
+
