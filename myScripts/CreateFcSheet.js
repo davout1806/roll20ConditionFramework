c04546824a144ef99d5699efdd661c1427ed3ff8
davout1805 <davout1805@gmail.com>
2013-09-30 09:29:08 -0400

    Removed "edit" character ability code Added combat abilities Added labels to separators

--- myScripts/CreateFcSheet.js
+++ myScripts/CreateFcSheet.js
@@ -19,6 +19,13 @@ DavoutSheet.attribs["spdFly"] = { name: "Flying-Speed", current: 0, max: 0 };
 DavoutSheet.attribs["spdSwim"] = { name: "Swimming-Speed", current: 0, max: 0 };
 DavoutSheet.attribs["spdBurrow"] = { name: "Burrow-Speed", current: 0, max: 0 };
 DavoutSheet.attribs["initBase"] = { name: "Initiative-Base" , current: 0, max: 0 };
+DavoutSheet.attribs["attMeleeBase"] = { name: "Att-Melee-Base" , current: 0, max: 0 };
+DavoutSheet.attribs["attHurlBase"] = { name: "Att-Hurl-Base" , current: 0, max: 0 };
+DavoutSheet.attribs["attRangeBase"] = { name: "Att-Range-Base" , current: 0, max: 0 };
+DavoutSheet.attribs["damMeleeBase"] = { name: "Dam-Melee-Base" , current: 0, max: 0 };
+DavoutSheet.attribs["damHurlBase"] = { name: "Dam-Hurl-Base" , current: 0, max: 0 };
+DavoutSheet.attribs["damRangeBase"] = { name: "Dam-Range-Base" , current: 0, max: 0 };
+
 DavoutSheet.attribs["acrobaticsBase"] = { name: "Acrobatics-Base", current: 0, max: 0 };
 DavoutSheet.attribs["athleticsBase"] = { name: "Athletics-Base", current: 0, max: 0 };
 DavoutSheet.attribs["blendBase"] = { name: "Blend-Base", current: 0, max: 0 };
@@ -47,27 +54,33 @@ DavoutSheet.saves["will"] = { name: "Will-Save", attribute: "Wis", bonus: Davout
 // TODO create code to generate ability rolls
 DavoutSheet.combat = [];
 DavoutSheet.combat["initiative"] = { name: "Initiative", attribute: "Dex", bonus: DavoutSheet.attribs["initBase"].name };
+DavoutSheet.combat["attackMelee"] = { name: "Attack-Melee", attribute: "Str", bonus: DavoutSheet.attribs["attMeleeBase"].name };
+DavoutSheet.combat["attackHurl"] = { name: "Attack-Hurl", attribute: "Dex", bonus: DavoutSheet.attribs["attRangeBase"].name };
+DavoutSheet.combat["attackRange"] = { name: "Attack-Range", attribute: "Dex", bonus: DavoutSheet.attribs["attRangeBase"].name };
+DavoutSheet.combat["damageMelee"] = { name: "Damage-Melee", attribute: "Str", bonus: DavoutSheet.attribs["damMeleeBase"].name };
+DavoutSheet.combat["damageHurl"] = { name: "Damage-Hurl", attribute: "Str", bonus: DavoutSheet.attribs["damHurlBase"].name };
+DavoutSheet.combat["damageRange"] = { name: "Damage-Range", attribute: "Dex", bonus: DavoutSheet.attribs["damRangeBase"].name };
 
 DavoutSheet.skills = [
-    { name: "Acrobatics", attribute: DavoutSheet.attribs["dex"].name, rank: DavoutSheet.attribs["acrobaticsBase"].name, acp: true },
-    { name: "Athletics", attribute: DavoutSheet.attribs["str"].name, rank: DavoutSheet.attribs["athleticsBase"].name, acp: true },
-    { name: "Blend", attribute: DavoutSheet.attribs["cha"].name, rank: DavoutSheet.attribs["blendBase"].name, acp: false },
-    { name: "Bluff", attribute: DavoutSheet.attribs["cha"].name, rank: DavoutSheet.attribs["bluffBase"].name, acp: false },
-    { name: "Crafting", attribute: DavoutSheet.attribs["int"].name, rank: DavoutSheet.attribs["craftingBase"].name, acp: false },
-    { name: "Haggle", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["haggleBase"].name, acp: false },
-    { name: "Impress", attribute: DavoutSheet.attribs["cha"].name, rank: DavoutSheet.attribs["impressBase"].name, acp: false },
-    { name: "Intimidate", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["intimidateBase"].name, acp: false },
-    { name: "Investigate", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["investigateBase"].name, acp: false },
-    { name: "Medicine", attribute: DavoutSheet.attribs["int"].name, rank: DavoutSheet.attribs["medicineBase"].name, acp: false },
-    { name: "Notice", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["noticeBase"].name, acp: false },
-    { name: "Prestidigitation", attribute: DavoutSheet.attribs["dex"].name, rank: DavoutSheet.attribs["prestidigitationBase"].name, acp: true },
-    { name: "Resolve", attribute: DavoutSheet.attribs["con"].name, rank: DavoutSheet.attribs["resolveBase"].name, acp: false },
-    { name: "Ride", attribute: DavoutSheet.attribs["dex"].name, rank: DavoutSheet.attribs["rideBase"].name, acp: true },
-    { name: "Search", attribute: DavoutSheet.attribs["int"].name, rank: DavoutSheet.attribs["searchBase"].name, acp: false },
-    { name: "Sense-Motive", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["senseMotiveBase"].name, acp: false },
-    { name: "Sneak", attribute: DavoutSheet.attribs["dex"].name, rank: DavoutSheet.attribs["sneakBase"].name, acp: true },
-    { name: "Survival", attribute: DavoutSheet.attribs["wis"].name, rank: DavoutSheet.attribs["survivalBase"].name, acp: false },
-    { name: "Tactics", attribute: DavoutSheet.attribs["int"].name, rank: DavoutSheet.attribs["tacticsBase"].name, acp: false }
+    { name: "Acrobatics", attribute: DavoutSheet.attribs["dex"].name, bonus: DavoutSheet.attribs["acrobaticsBase"].name, acp: true },
+    { name: "Athletics", attribute: DavoutSheet.attribs["str"].name, bonus: DavoutSheet.attribs["athleticsBase"].name, acp: true },
+    { name: "Blend", attribute: DavoutSheet.attribs["cha"].name, bonus: DavoutSheet.attribs["blendBase"].name, acp: false },
+    { name: "Bluff", attribute: DavoutSheet.attribs["cha"].name, bonus: DavoutSheet.attribs["bluffBase"].name, acp: false },
+    { name: "Crafting", attribute: DavoutSheet.attribs["int"].name, bonus: DavoutSheet.attribs["craftingBase"].name, acp: false },
+    { name: "Haggle", attribute: DavoutSheet.attribs["wis"].name, bonus: DavoutSheet.attribs["haggleBase"].name, acp: false },
+    { name: "Impress", attribute: DavoutSheet.attribs["cha"].name, bonus: DavoutSheet.attribs["impressBase"].name, acp: false },
+    { name: "Intimidate", attribute: DavoutSheet.attribs["wis"].name, bonus: DavoutSheet.attribs["intimidateBase"].name, acp: false },
+    { name: "Investigate", attribute: DavoutSheet.attribs["wis"].name, bonus: DavoutSheet.attribs["investigateBase"].name, acp: false },
+    { name: "Medicine", attribute: DavoutSheet.attribs["int"].name, bonus: DavoutSheet.attribs["medicineBase"].name, acp: false },
+    { name: "Notice", attribute: DavoutSheet.attribs["wis"].name, bonus: DavoutSheet.attribs["noticeBase"].name, acp: false },
+    { name: "Prestidigitation", attribute: DavoutSheet.attribs["dex"].name, bonus: DavoutSheet.attribs["prestidigitationBase"].name, acp: true },
+    { name: "Resolve", attribute: DavoutSheet.attribs["con"].name, bonus: DavoutSheet.attribs["resolveBase"].name, acp: false },
+    { name: "Ride", attribute: DavoutSheet.attribs["dex"].name, bonus: DavoutSheet.attribs["rideBase"].name, acp: true },
+    { name: "Search", attribute: DavoutSheet.attribs["int"].name, bonus: DavoutSheet.attribs["searchBase"].name, acp: false },
+    { name: "Sense-Motive", attribute: DavoutSheet.attribs["wis"].name, bonus: DavoutSheet.attribs["senseMotiveBase"].name, acp: false },
+    { name: "Sneak", attribute: DavoutSheet.attribs["dex"].name, bonus: DavoutSheet.attribs["sneakBase"].name, acp: true },
+    { name: "Survival", attribute: DavoutSheet.attribs["wis"].name, bonus: DavoutSheet.attribs["survivalBase"].name, acp: false },
+    { name: "Tactics", attribute: DavoutSheet.attribs["int"].name, bonus: DavoutSheet.attribs["tacticsBase"].name, acp: false }
 ];
 
 DavoutSheet.processAttribs = function processAttribs(character) {
@@ -100,11 +113,11 @@ DavoutSheet.createSaveAction = function createSaveAction(save) {
     return saveAction;
 };
 
-DavoutSheet.createSkillAction = function createSkillAction(skill, charType) {
+DavoutSheet.createSkillOrCombatAction = function createSkillOrCombatAction(skill, charType) {
     var abilityAction = "/w gm @{Name} ";
     abilityAction += skill.name;
     abilityAction += ": [[floor(@{" + skill.attribute + "}/2-5)";
-    abilityAction += "+@{" + skill.rank + "}";
+    abilityAction += "+@{" + skill.bonus + "}";
     abilityAction += DavoutSheet.getSkillBonus(skill, charType);
     if (skill.acp) {
         abilityAction += "+@{" + DavoutSheet.attribs["acp"].name + "}";
@@ -124,19 +137,19 @@ DavoutSheet.retrieveUserModifiers = function retrieveUserModifiers(currentSkill,
     return modifier;
 };
 
-DavoutSheet.createSeparator = function createSeparator(character) {
+DavoutSheet.createSeparator = function createSeparator(character, label) {
     var placeHolder = findObjs({
-        name: "--------------------------",
+        name: "----" + label + "----",
         description: "",
-        action: "/w gm --------------------------",
+        action: "/w gm ----" + label + "----",
         type: "ability",
         characterid: character.id
     }, {caseInsensitive: true});
     if (placeHolder.length < 1) {
         createObj("ability", {
-            name: "--------------------------",
+            name: "----" + label + "----",
             description: "",
-            action: "/w gm --------------------------",
+            action: "/w gm ----" + label + "----",
             characterid: character.id
         });
     }
@@ -145,54 +158,37 @@ DavoutSheet.createSeparator = function createSeparator(character) {
 DavoutSheet.processSkills = function processSkills(character, charType) {
     for (var save in DavoutSheet.saves) {
         var saveAction = DavoutSheet.createSaveAction(DavoutSheet.saves[save]);
-        var savingthrows = findObjs({
-            name: DavoutSheet.saves[save].name,
-            description: "",
-            type: "ability",
-            characterid: character.id
-        }, {caseInsensitive: true});
-        if (savingthrows.length > 0) {
-            _.each(savingthrows, function(save){
-                save.set("action", saveAction);
-            });
-        } else {
             createObj("ability", {
                 name: DavoutSheet.saves[save].name,
                 description: "",
                 action: saveAction,
                 characterid: character.id
             });
-        }
     };
 
-    DavoutSheet.createSeparator(character);
+    DavoutSheet.createSeparator(character, "Combat");
+
+    for (var combat in DavoutSheet.combat) {
+        var combatAction = DavoutSheet.createSkillOrCombatAction(DavoutSheet.combat[combat], charType);
+        createObj("ability", {
+            name: DavoutSheet.combat[combat].name,
+            description: "",
+            action: combatAction,
+            characterid: character.id
+        });
+    };
+
+    DavoutSheet.createSeparator(character, "Skills");
     
     for (var skill in DavoutSheet.skills) {
-        var abilityAction = DavoutSheet.createSkillAction(DavoutSheet.skills[skill], charType);
-        var abilities = findObjs({
-            name: DavoutSheet.skills[skill].name,
-            type: "ability",
-            description: "",
-            characterid: character.id
-        }, {caseInsensitive: true});
-        if (abilities.length > 0) {
-            _.each(abilities, function(ability) {
-                var userModifier = DavoutSheet.retrieveUserModifiers(DavoutSheet.skills[skill], ability.get("action"));
-                //log("Retrieved User-Modifier: " + userModifier);
-                abilityAction += userModifier;
-                ability.set("action", abilityAction);
-            })
-        } else {
+        var abilityAction = DavoutSheet.createSkillOrCombatAction(DavoutSheet.skills[skill], charType);
             createObj("ability", {
                 name: DavoutSheet.skills[skill].name,
                 description: "",
                 action: abilityAction,
                 characterid: character.id
             });
-        }
     };
-
-    DavoutSheet.createSeparator(character);
 };
 
 on("ready", function() {
