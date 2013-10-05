9b9d4c7148ac4764fdde08abfef4964946d44242
davout1805 <davout1805@gmail.com>
2013-10-05 19:33:20 -0400

    Added on("ready", function () back

--- main/prod/ConditionsByToken.js
+++ main/prod/ConditionsByToken.js
@@ -153,5 +153,55 @@ Davout.Conditions.command._manageCondition = function (actionType, selected, con
 
 on("destroy:token", Davout.Conditions.onTokenDestroyedEvent);
 
+on("ready", function () {     // Requires community.command
+    if (community == undefined || !("command" in community)) {
+        log("You must have community.command installed in a script tab before the tab this script is in to use pigalot.requests.phrases.");
+        throw "Can't find community.command!";
+    }
+    if (DavoutUtils == undefined) {
+        log("You must have DavoutUtils installed in a script tab before the tab this script is in to use.");
+        throw "Can't find DavoutUtils!";
+    }
+
+    var addCommand = {};
+    addCommand.minArgs = 1;
+    addCommand.maxArgs = 1;
+    addCommand.typeList = [];
+    addCommand.typeList = ["str"];
+    addCommand.syntax = "!cond_add ConditionName";
+    addCommand.handle = function (args, who, isGm, msg) {
+        if (DavoutUtils.checkForSelectionAndMsgIfNot(msg.selected, "/w gm nothing is selected", false, "") && isGm) {
+            DavoutConditions.command._manageCondition("ADD", msg.selected, args[0].value);
+        }
+    };
+    community.command.add("cond_add", addCommand);
+
+    var removeCommand = {};
+    removeCommand.minArgs = 1;
+    removeCommand.maxArgs = 1;
+    removeCommand.typeList = [];
+    removeCommand.typeList = ["str"];
+    removeCommand.syntax = "!cond_del ConditionName";
+    removeCommand.handle = function (args, who, isGm, msg) {
+        if (DavoutUtils.checkForSelectionAndMsgIfNot(msg.selected, "/w gm nothing is selected", false, "") && isGm) {
+            DavoutConditions.command._manageCondition("DEL", msg.selected, args[0].value);
+        }
+    };
+    community.command.add("cond_del", removeCommand);
+
+    var showCommand = {};
+    showCommand.minArgs = 0;
+    showCommand.maxArgs = 0;
+    showCommand.typeList = [];
+    showCommand.typeList = ["str"];
+    showCommand.syntax = "!cond_show";
+    showCommand.handle = function (args, who, isGm, msg) {
+        if (DavoutUtils.checkForSelectionAndMsgIfNot(msg.selected, "/w gm nothing is selected", false, "") && isGm) {
+            DavoutConditions.command._manageCondition("SHOW", msg.selected);
+        }
+    };
+    community.command.add("cond_show", showCommand);
+});
+
 // ConditionsByToken
 
