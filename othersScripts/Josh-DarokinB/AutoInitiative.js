eab65ea4232a78234ce105a5949896f4dc5bc5ad
davout1805 <davout1805@gmail.com>
2013-10-19 13:15:27 -0400

    Potential AutoInit. Turn tracker is buggy even with Josh's original version.

--- othersScripts/Josh-DarokinB/AutoInitiative.js
+++ othersScripts/Josh-DarokinB/AutoInitiative.js
@@ -4,21 +4,22 @@
  */
 var Combat_Begins = Combat_Begins || {};
 
-Combat_Begins.fourth_ed = false; //If using 4th edition, it takes 1/2 'Level' and adds it for initiative
-Combat_Begins.statName = new Array ("Dex"); //Stats to be added to roll, commas between values
+Combat_Begins.statName = "Dex"; //Stats to be added to roll, commas between values
+Combat_Begins.modifiers = new Array("Initiative-Base"); //Stats to be added to roll, commas between values
 Combat_Begins.rollValue = 20; //rolling 1d20, change if you roll 1dXX
 Combat_Begins.sendChat = true; //True if you want the chat log to show their results
+Combat_Begins.command = Combat_Begins.command || {};
 
-on("chat:message", function(msg) {
-    if (msg.type == "api" && msg.content.indexOf("!CombatBegins") !== -1 && msg.who.indexOf("(GM)") !== -1) {
+Combat_Begins.command._combatBegins = function (msg) {
+    if (msg.type == "api" && msg.who.indexOf("(GM)") !== -1) {
 
-        Campaign().set("initiativepage", true );
+        Campaign().set("initiativepage", true);
 
         if (Combat_Begins.sendChat == true) {
             sendChat("", "/desc Combat Begins!");
         }
 
-        if(Campaign().get("turnorder") == "") turnorder = []; //NOTE: We check to make sure that the turnorder isn't just an empty string first. If it is treat it like an empty array.
+        if (Campaign().get("turnorder") == "") turnorder = []; //NOTE: We check to make sure that the turnorder isn't just an empty string first. If it is treat it like an empty array.
         else turnorder = JSON.parse(Campaign().get("turnorder"));
 
         var mod = 0;
@@ -31,7 +32,7 @@ on("chat:message", function(msg) {
             _pageid: Campaign().get("playerpageid"),
         }, {caseInsensitive: true});
 
-        _.each(msg.selected, function(selected) {
+        _.each(msg.selected, function (selected) {
             modlist.length = 0;
             mod = 0;
 
@@ -41,23 +42,26 @@ on("chat:message", function(msg) {
 
                 var currChar = getObj("character", obj.get("represents"));
 
-                _.each(Combat_Begins.statName, function(stat) {
+                var attribObj = findObjs({
+                    name: Combat_Begins.statName,
+                    _type: "attribute",
+                    _characterid: currChar.id,
+                }, {caseInsensitive: true})[0];
+
+                var attribModifier = Number(Math.floor(attribObj.get("current") / 2)) - 5;
+                mod += attribModifier;
+                modlist.push({name: Combat_Begins.statName, value: attribModifier || 0 });
+
+                _.each(Combat_Begins.modifiers, function (attributeName) {
                     //cycle through each stat and add it to mod
-                    var modifier = findObjs({
-                        name: stat,
+                    var attribObj = findObjs({
+                        name: attributeName,
                         _type: "attribute",
                         _characterid: currChar.id,
                     }, {caseInsensitive: true});
 
-                    if (modifier.length !== 0 ) {
-                        if (Combat_Begins.fourth_ed == true && modifier.get("name") == "Level") {
-                            mod = mod + (Number(floor(modifier[0].get("current") / 2)))
-                            modlist.push({name: stat, value: Number(floor(modifier[0].get("current") / 2)) })
-                        } else {
-                            mod = mod + (Number(modifier[0].get("current")) || 0);
-                            modlist.push({name: stat, value: Number(modifier[0].get("current") || 0) })
-                        }
-                    }
+                    mod = mod + (Number(attribObj[0].get("current")) || 0);
+                    modlist.push({name: attributeName, value: Number(attribObj[0].get("current") || 0) })
                 });
             } else {
                 mod = 0;
@@ -67,12 +71,12 @@ on("chat:message", function(msg) {
             var init = (rolled + mod);
             var string = "";
 
-            if (Combat_Begins.sendChat == true ) {
+            if (Combat_Begins.sendChat == true) {
 
-                _.each(modlist, function(obj) {
+                _.each(modlist, function (obj) {
                     string = string + (" + " + obj.value + " " + obj.name) || "";
                 })
-                sendChat("",  obj.get("name") + " rolled a " + init + " (" + rolled.toString() + " rolled " + string + ")");
+                sendChat("", obj.get("name") + " rolled a " + init + " (" + rolled.toString() + " rolled " + string + ")");
             }
 
             var Added = false;
@@ -80,7 +84,7 @@ on("chat:message", function(msg) {
 
 
             //search for if it is existing
-            while (loop < turnorder.length && Added == false){
+            while (loop < turnorder.length && Added == false) {
                 if (turnorder[loop].id == selected._id) {
                     turnorder[loop].pr = init;
                     Added = true;
@@ -107,19 +111,63 @@ on("chat:message", function(msg) {
         Campaign().set("turnorder", JSON.stringify(turnorder));
 
     }
+};
 
-    if (msg.type == "api" && msg.content.indexOf("!CombatEnds") !== -1) {
+Combat_Begins.command._combatEnds = function (msg) {
+        Campaign().set("turnorder", false);
+        Campaign().set("initiativepage", false);
+};
 
-        Campaign().set("turnorder", false );
-        Campaign().set("initiativepage", false );
-        if (MovementTracker.MovementTracker == true) { ResetAllPins() };
+Combat_Begins.command._combatSuspend = function (msg) {
+        if (Campaign().get("initiativepage", Campaign().get("playerpageid"))) {
+            Campaign().set("initiativepage", false)
+        }
+        else {
+            Campaign().set("initiativepage", Campaign().get("playerpageid"))
+        }
+};
+
+on("ready", function () {
+    if (community == undefined || !("command" in community)) {
+        log("You must have community.command installed in a script tab before the tab this script is in to use pigalot.requests.phrases.");
+        throw "Can't find community.command!";
+    }
+
+    var showCommand = {};
+    showCommand.minArgs = 0;
+    showCommand.maxArgs = 0;
+    showCommand.typeList = [];
+    showCommand.typeList = ["str"];
+    showCommand.syntax = "!combatBegins";
+    showCommand.handle = function (args, who, isGm, msg) {
+        if (Davout.Utils.checkForSelectionAndMsgIfNot(msg.selected, "/w gm nothing is selected", false, "") && isGm) {
+            Combat_Begins.command._combatBegins(msg);
+        }
     };
+    community.command.add("combatBegins", showCommand);
 
-    if (msg.type == "api" && msg.content.indexOf("!CombatSuspend") !== -1) {
-
-        var initTracker = "";
-        if (Campaign().get("initiativepage", Campaign().get("playerpageid") ) ) { Campaign().set("initiativepage", false ) }
-        else { Campaign().set("initiativepage", Campaign().get("playerpageid") ) };
-
+    var showCommand = {};
+    showCommand.minArgs = 0;
+    showCommand.maxArgs = 0;
+    showCommand.typeList = [];
+    showCommand.typeList = ["str"];
+    showCommand.syntax = "!combatEnds";
+    showCommand.handle = function (args, who, isGm, msg) {
+            Combat_Begins.command._combatEnds(msg);
     };
+    community.command.add("combatEnds", showCommand);
+
+    var showCommand = {};
+    showCommand.minArgs = 0;
+    showCommand.maxArgs = 0;
+    showCommand.typeList = [];
+    showCommand.typeList = ["str"];
+    showCommand.syntax = "!combatSuspend";
+    showCommand.handle = function (args, who, isGm, msg) {
+        Combat_Begins.command._combatSuspend(msg);
+    };
+    community.command.add("combatSuspend", showCommand);
 });
+
+// AutoInitiative
+
