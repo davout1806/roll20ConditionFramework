e5c735a8e86ae96467fb0c97ea3eb7536ce041e0
davout1805 <davout1805@gmail.com>
2013-11-24 10:22:14 -0500

    Comments regarding project functionality

--- main/prod/Conditions.js
+++ main/prod/Conditions.js
@@ -1,3 +1,49 @@
+/**
+ *
+ * A key design goal: Conditions are associated to individual tokens not characters. This allows the use of multiple tokens to be associated to a single character while each token can have different conditions. But each token needs to be associated a character.
+
+ A Condition contains a list of Effects. An Effect can be associated to either an attribute, an action or a reaction to an action (such as "vs melee attack")
+
+ Each Effect has the following (all of which are configurable):
+
+ a flag that determines whether the effect will prohibit the associated action
+ optional modifier
+ optional note string
+
+ An Action is something the token is trying to do that requires a successful roll to succeed. This could be an attack or skill check. An Action has the following (all of which are configurable):
+
+ name of attribute (as in D&D attribute ex: Strength) from the character that is associated to the action. For example, Melee Attack action could be STR
+ name of attribute (as in a character's rank in a skill) from the character that is associated to the action. For example, Sneak action could be Sneak_Skill
+ flag if APC effects action
+ name of APC attribute
+
+ Additional Features:
+
+ GM can add/remove condition on all selected tokens. [done]
+ Ability to configure what Effects are associated with a given Condition. [done]
+ Ability to set timer on a specific condition on a specific token.
+ Ability to stack multiple occurrences of a Condition on the same token up to a configurable number. [done]
+ Ability to set a condition to be automatically added to token when a specific Condition has reached it max stack size. [done]
+ Apply optional Effect modifier to result. [done]
+ If Effect is associated to an attribute, it will be applied to that attribute of the character, which then affects the result. [done]
+ Code to define the conditions and their effects is separate from code that tracks and actually applies effects. [done]
+ Ability to select 1 or more targets of an action.
+ Compute roll result for each target.
+
+ Required design specifications:
+ 1. Allow multiple tokens to use the same character.
+ 2. Allow conditions to be assigned individually to tokens even if they share the same character.
+
+ Desired design specifications:
+ 1. Quickly perform action of a token, preferably without having to open character sheet (ALT+double click)
+
+ * state.Davout.TokensWithConditionObj[id] where id is token id
+ *
+ * Since tokens might be associated to 0, 1, or more characters, it is not possible to adjust the ratings.
+ * Therefore, must apply effects on rolls.
+ *
+ */
+
 Davout = (Davout || {});
 Davout.ConditionFW = Davout.ConditionFW || {};
 
